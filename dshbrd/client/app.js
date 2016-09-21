a10c1ee6e49f3daecb15318fc1a6ca4ac1412ad9
thomas dove <thomas dove>
2016-09-21 14:12:37 +0100

    Got twitter and fb working

--- dshbrd/client/app.js
+++ dshbrd/client/app.js
@@ -90,20 +90,62 @@ Template.loginForm.events({
         event.preventDefault();
         var usernameVar = event.target.loginUsername.value;
         var passwordVar = event.target.loginPassword.value;
-        Meteor.loginWithPassword(usernameVar, passwordVar);
+        Meteor.loginWithPassword(usernameVar, passwordVar , function(err){
+            if (err) {
+              console.log('login failed');
+              console.log(err);
+            } else {
+              console.log('login success');
+            }
+          });
+
     }
 });
+
+Template.header.onCreated( function() {
+  Session.set("temp","loginForm");
+})
 
 Template.header.events({
     'click .profile_pic': function(e) {
         e.preventDefault();
         if( $(".profile_menu").css('opacity') == 0){
-          console.log("butts");
           $('.profile_menu').css('opacity', 1);
         }
         else
         {
           $('.profile_menu').css('opacity', 0);
         }
+    },
+
+    'click #registerBtn': function(e) {
+        e.preventDefault();
+        Session.set("temp", "registerForm");
+    },
+
+    'click #signinBtn': function(e) {
+        e.preventDefault();
+        Session.set("temp", "loginForm");
+    },
+
+    'click #logoutBtn': function(e) {
+        e.preventDefault();
+        Meteor.logout();
+    },
+
+    'click #facebookLoginBtn': function(e) {
+        e.preventDefault();
+        Meteor.loginWithFacebook({});
+    },
+
+    'click #twitterLoginBtn': function(e) {
+        e.preventDefault();
+        Meteor.loginWithTwitter({});
+    }
+});
+
+Template.header.helpers({
+    temp: function(){
+        return Session.get('temp');
     }
 });
---
+++ dshbrd/server/social-config.js
@@ -0,0 +1,13 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook',
+    service : 'twitter'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1762655590660800',
+    secret: '4f8cc8ff3e7ee3db1dfaeedd9946003f',
+    service     : 'twitter',
+    consumerKey : 'p4gMdJS68CJC3WBMNUrylR3CW',
+    secret      : 'W84GC6hsVbIU0ly4q5a55uUsfdM8aZ7MU5R6fkmAIHBfRYxPdq'
+});
